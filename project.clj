bacb5bc7db4818abf7985c8d1d3aff5120803e93
oakes <zach@sekao.net>
2020-08-31 18:06:19 -0400

    Add jar exclusions

--- project.clj
+++ project.clj
@@ -1,3 +1,7 @@
 (defproject odoyle-rum-todo "0.1.0-SNAPSHOT"
   :aot [odoyle-rum-todo.start]
-  :main odoyle-rum-todo.start)
+  :main odoyle-rum-todo.start
+  :exclusions [cljsjs/react
+               cljsjs/react-dom
+               sablono
+               ring/ring-devel])
